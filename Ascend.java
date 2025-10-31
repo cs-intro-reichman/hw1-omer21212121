d66d206b16b37e7999241109583ab62d1400255b
Omer Eyal <omer.eyal@post.runi.ac.il>
2025-10-31 20:34:50 +0200

    completed part 4 of the assignment

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,14 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		double a = Math.random() * Integer.parseInt(args[0]);
+	    double b = Math.random() * Integer.parseInt(args[0]);
+		double c = Math.random() * Integer.parseInt(args[0]);
+		double smallNum = Math.min (a, Math.min(b,c));
+		double bigNum = Math.max(a, Math.max(b,c));
+		double mediumNum = (a+b+c)-(smallNum +bigNum);
+		System.out.println((int) a + " " + (int) b + " " + (int) c);
+		System.out.println((int) smallNum + " " + (int) mediumNum + " " + (int) bigNum);
+
 	}
 }
