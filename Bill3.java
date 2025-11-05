6881f406fcd7730ec52836553688f8223100e310
Omer Eyal <omer.eyal@post.runi.ac.il>
2025-11-05 14:22:17 +0200

    fix2

--- Bill3.java
+++ Bill3.java
@@ -6,8 +6,8 @@ public class Bill3 {
 		String name2 = args[1];
 		String name3 = args[2];
 		int billAmount = Integer.parseInt(args[3]);
-		double splitBillAmount = Math.ceil(billAmount/3);
-		System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 + ": pay " + splitBillAmount + " Shekels each.");
+		double splitBillAmount = Math.ceil(billAmount/3.0);
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + splitBillAmount + " Shekels each");
 
 
 	    // Replace this comment with the rest of your code   
